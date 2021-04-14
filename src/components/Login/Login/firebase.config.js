839eba75d76943715119eb96165572d5b9fb23b5
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-04-14 14:09:52 +0600

    first commit

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA72ga2nUnKl8I9hpvWxcPOsqEftY_Yc78",
+  authDomain: "doctor-s-portal-e9d4f.firebaseapp.com",
+  projectId: "doctor-s-portal-e9d4f",
+  storageBucket: "doctor-s-portal-e9d4f.appspot.com",
+  messagingSenderId: "984096639903",
+  appId: "1:984096639903:web:22b6f93af46b25a7964baa"
+};
+  export default firebaseConfig;
